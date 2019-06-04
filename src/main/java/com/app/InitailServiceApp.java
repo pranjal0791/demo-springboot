3b9fe796a0289e879e3f5567cb6a4cb3c4c8668e
pranjalsaikiaws <pranjalsaikiaaws@gmail.com>
2019-06-04 15:57:21 +0530

    Revert "Added tomcat dependency"
    This reverts commit 9369af9abf9a3c8edb2ca51c28a35d754b9ec8a0.

--- src/main/java/com/app/InitailServiceApp.java
+++ src/main/java/com/app/InitailServiceApp.java
@@ -2,12 +2,11 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.app")
-public class InitailServiceApp extends SpringBootServletInitializer{
+public class InitailServiceApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
