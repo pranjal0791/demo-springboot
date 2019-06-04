9369af9abf9a3c8edb2ca51c28a35d754b9ec8a0
pranjalsaikiaws <pranjalsaikiaaws@gmail.com>
2019-06-04 15:25:38 +0530

    Added tomcat dependency

--- src/main/java/com/app/InitailServiceApp.java
+++ src/main/java/com/app/InitailServiceApp.java
@@ -2,11 +2,12 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.app")
-public class InitailServiceApp {
+public class InitailServiceApp extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
