0c52383e51329ef126a7afd7eff4e9130e2d3e0e
pranjalsaikiaws <pranjalsaikiaaws@gmail.com>
2019-06-04 09:39:30 +0530

    Added Unit test case

---
+++ src/test/java/com/app/test/TestWebApp.java
@@ -0,0 +1,35 @@
+package com.app.test;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class TestWebApp extends SpringBootDemoTest{
+	
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+	
+	@Test
+	public void testEmployee() throws Exception {
+		mockMvc.perform(get("/employee/all")).andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;charset=UTF-8"))
+				;
+
+	}
+
+}
